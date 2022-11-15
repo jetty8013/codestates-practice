e4756a64ce953b69eb364cd6137f7b1663557beb
jetty8013 <jetty8013@gmail.com>
2022-11-15 23:30:33 +0900

    다이얼 ? 추가

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
-console.log(agoraStatesDiscussions);
+// console.log(agoraStatesDiscussions);
 
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
@@ -43,6 +43,8 @@ const convertToDiscussion = (obj) => {
 
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
+  li.addEventListener("mouseover", tabOver)
+  li.addEventListener("mouseout", tabOut)
   return li;
 };
 
@@ -52,6 +54,7 @@ const addDial = document.querySelector("section.dial_number");
 const render = (element) => {
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
     const writerName2 = document.createElement("span");
+    writerName2.id = "dial_"+i
     writerName2.textContent = i
     addDial.append(writerName2)
     element.append(convertToDiscussion(agoraStatesDiscussions[i]));
@@ -61,9 +64,44 @@ const render = (element) => {
 
 const scroll_discussion = document.querySelector("section.discussion__wrapper");
 
-scroll_discussion.onmouseover=function(){
-  // console.log(getIndex(this))
+
+scroll_discussion.addEventListener("scroll" , function(){
+  const percentScroll = Math.floor(( 
+    document.querySelector("section.discussion__wrapper").scrollTop /
+    (document.querySelector("section.discussion__wrapper").scrollHeight - 
+    document.querySelector("section.discussion__wrapper").clientHeight)
+  ) * 100);
+
+  addDial.scrollLeft= (addDial.scrollWidth-addDial.clientWidth)*(percentScroll/100)
+})
+
+let currentTab; // 클릭이벤트가 전달된 엘리먼트
+let parentEl; // 연결된 엘리먼트의 부모
+let childEl; // 부모 엘리먼트의 자식 엘리먼트들
+let tabIndex; // 연결된 엘리먼트의 인덱스
+
+const tabOver = (tabEvents)=>{
+	currentTab = tabEvents.currentTarget;
+  parentEl = currentTab.parentElement;
+	childEl = parentEl.children;
+	tabIndex = Array.from(childEl).indexOf(currentTab);
+  document.querySelector("#dial_"+tabIndex).style.color = "#00ABB3"
+  document.querySelector("#dial_"+tabIndex).style.transform = "scale(3)"
+  document.querySelector("#dial_"+tabIndex).style.fontWeight = "800"
+  document.querySelector("#dial_"+tabIndex).style.margin = "6px"
 }
+
+const tabOut = (tabEvents)=>{
+	currentTab = tabEvents.currentTarget;
+  parentEl = currentTab.parentElement;
+	childEl = parentEl.children;
+	tabIndex = Array.from(childEl).indexOf(currentTab);
+  document.querySelector("#dial_"+tabIndex).style.color = "#3C4048"
+  document.querySelector("#dial_"+tabIndex).style.transform = "scale(1)"
+  document.querySelector("#dial_"+tabIndex).style.fontWeight = "300"
+  document.querySelector("#dial_"+tabIndex).style.margin = "3px"
+}
+
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
