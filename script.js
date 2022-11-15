91a73cc3d79372b96497bd7211d7e25338fb3e7d
jetty8013 <jetty8013@gmail.com>
2022-11-15 17:04:49 +0900

    커밋

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
 console.log(agoraStatesDiscussions);
 
+
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
@@ -14,7 +15,31 @@ const convertToDiscussion = (obj) => {
   discussionAnswered.className = "discussion__answered";
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  const avatarImg = document.createElement('img');
+  avatarImg.className = "discussion__avatar--img"
+  avatarImg.src = obj.avatarUrl;
+  avatarImg.alt = 'avatar of ' + obj.author;
+  avatarWrapper.append(avatarImg)
+  
+  const agoraTtitle = document.createElement("h2");
+  agoraTtitle.className = "discussion__title";
+  discussionContent.append(agoraTtitle);
 
+  const agoraLink = document.createElement("a");
+  agoraLink.href = obj.url;
+  agoraLink.textContent = obj.title;
+  agoraTtitle.append(agoraLink);
+
+  const writerName = document.createElement("div");
+  discussionContent.append(writerName);
+  
+  writerName.classList.add("discussion__information");
+  writerName.textContent = `${obj.author} /  ${new Date(obj.createdAt).toLocaleString("ko-KR")}`;
+
+  const isAnswerd = document.createElement("p");
+  isAnswerd.className = "discussion__answered"
+  isAnswerd.textContent = obj.answer ? "☑" : "☒";
+  discussionAnswered.append(isAnswerd);
 
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
@@ -29,6 +54,27 @@ const render = (element) => {
   return;
 };
 
+
+
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+const submit = document.getElementById('submit')
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const obj = {
+    id: "id",
+    createdAt: new Date(),
+    title: document.getElementById('usertitle').value,
+    url: "https://github.com/codestates-seb/agora-states-fe/discussions/45",
+    author: document.getElementById('username').value,
+    answer: null,
+    avatarUrl:
+      "https://avatars.githubusercontent.com/u/112256786?v=4",
+  };
+
+  ul.prepend(convertToDiscussion(obj))
+});
